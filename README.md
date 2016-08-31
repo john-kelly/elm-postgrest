16efa0749425561dc0374ec2bb266e6b5ad32cb2
john kelly <johnkelly@codehs.com>
2016-08-31 11:09:22 -0700

    update name of new pkg

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-DSL for building complex HTTP queries.
+DEPRECATED: Renamed to http://package.elm-lang.org/john-kelly/elm-query
---
+++ src/Rest.elm
@@ -0,0 +1,10 @@
+module Rest exposing (deprecated)
+
+{-| DEPRECATED: Renamed to http://package.elm-lang.org/john-kelly/elm-query
+# Functions
+@docs deprecated
+-}
+
+
+deprecated =
+    1
