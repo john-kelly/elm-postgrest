18fd2077434ef33eebe0a55be90ac1e3c99c9141
john kelly <johnkelly@codehs.com>
2017-02-20 19:00:49 -0800

    simple

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,8 +6,4 @@ import Expect
 
 all : Test
 all =
-    describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Test" <| \() -> Expect.equal True True
-            ]
-        ]
+    test "Test" <| \() -> Expect.pass
